6a760ef3dc3e9f790f63a160602491d325142875
rebelcl0ud <jovanka.placeres@gmail.com>
2018-12-25 02:33:37 -0800

    add: weeks/hours inputs and goBack button; value/onChange props on inputs and onClick props on buttons

--- src/Home.js
+++ src/Home.js
@@ -15,9 +15,22 @@ export default class Home extends Component {
          		<h1>Salary to Hourly</h1>
 
          		<label>Salary</label>
-	          <input type="text" name="salary" placeholder="$"/>
+	          <input type="text" name="salary" placeholder="$" 
+	          	value={this.props.globalState.salary}
+	          	onChange={this.props.onSalaryInputChange}/>
+
+	          <label>Weeks</label>
+	          <input type="text" name="weeks" placeholder="# of weeks"
+	          	value={this.props.globalState.weeks}
+	          	onChange={this.props.onWeeksInputChange}/>
+
+	          <label>Hours</label>
+	          <input type="text" name="hours" placeholder="# of hours"
+	          	value={this.props.globalState.hours}
+	          	onChange={this.props.onHoursInputChange}/>
 	          
-						<button type="submit">Get Hourly Rate</button>
+						<button type="submit" onClick={this.props.salaryToHourly}>Get Hourly Rate</button>
+         		<button type="submit" onClick={this.props.goBackClear}>Check Another Salary</button>
          	</div>
         </div>
       </section>
