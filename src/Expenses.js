d3337f62e1f8293012f2c95f12ebacaef61e5e9e
rebelcl0ud <jovanka.placeres@gmail.com>
2018-12-26 03:10:06 -0800

    update: more build out, 10 slotson each column, total slots at bottom

--- src/Expenses.js
+++ src/Expenses.js
@@ -8,20 +8,70 @@ export default class Expenses extends Component {
           <h1>Lifestyle Upkeep</h1> 
           <div className="form col-md-3">
             <label>Daily</label>
-            <input type="text" name="salary" placeholder="$" />
+            <input type="text" name="daily" placeholder="$" />
+            <input type="text" name="daily" placeholder="$" />
+            <input type="text" name="daily" placeholder="$" />
+            <input type="text" name="daily" placeholder="$" />
+            <input type="text" name="daily" placeholder="$" />
+            <input type="text" name="daily" placeholder="$" />
+            <input type="text" name="daily" placeholder="$" />
+            <input type="text" name="daily" placeholder="$" />
+            <input type="text" name="daily" placeholder="$" />
+            <input type="text" name="daily" placeholder="$" />
           </div>
           <div className="form col-md-3">
             <label>Yearly</label>
-            <input type="text" name="salary" placeholder="$" />
+            <input type="text" name="yearly" placeholder="$" />
+            <input type="text" name="yearly" placeholder="$" />
+            <input type="text" name="yearly" placeholder="$" />
+            <input type="text" name="yearly" placeholder="$" />
+            <input type="text" name="yearly" placeholder="$" />
+            <input type="text" name="yearly" placeholder="$" />
+            <input type="text" name="yearly" placeholder="$" />
+            <input type="text" name="yearly" placeholder="$" />
+            <input type="text" name="yearly" placeholder="$" />
+            <input type="text" name="yearly" placeholder="$" />
           </div>
            <div className="form col-md-3">
             <label>Monthly</label>
-            <input type="text" name="salary" placeholder="$" />
+            <input type="text" name="monthly" placeholder="$" />
+            <input type="text" name="monthly" placeholder="$" />
+            <input type="text" name="monthly" placeholder="$" />
+            <input type="text" name="monthly" placeholder="$" />
+            <input type="text" name="monthly" placeholder="$" />
+            <input type="text" name="monthly" placeholder="$" />
+            <input type="text" name="monthly" placeholder="$" />
+            <input type="text" name="monthly" placeholder="$" />
+            <input type="text" name="monthly" placeholder="$" />
+            <input type="text" name="monthly" placeholder="$" />
           </div>
           <div className="form col-md-3">
             <label>Expenses</label>
+            <input type="text" name="expenses" placeholder="Rent" />
+            <input type="text" name="expenses" placeholder="Phone" />
+            <input type="text" name="expenses" placeholder="Transportation" />
+            <input type="text" name="expenses" placeholder="Groceries" />
+            <input type="text" name="expenses" placeholder="Subscriptions" />
+            <input type="text" name="expenses" placeholder="Item" />
+            <input type="text" name="expenses" placeholder="Item" />
+            <input type="text" name="expenses" placeholder="Item" />
+            <input type="text" name="expenses" placeholder="Item" />
+            <input type="text" name="expenses" placeholder="Item" />
+          </div>
+
+          <div className="form col-md-3">
+            <label>Daily Total</label>
             <input type="text" name="salary" placeholder="$" />
           </div>
+          <div className="form col-md-3">
+            <label>Yearly Total</label>
+            <input type="text" name="salary" placeholder="$" />
+          </div>
+           <div className="form col-md-3">
+            <label>Monthly Total</label>
+            <input type="text" name="salary" placeholder="$" />
+          </div>
+
           <button type="submit" onClick={this.props.goBackClear}>Check Another Salary</button>
           <button type="submit" className="register"> Register to save Expenses</button>
         </div>
